2e51d416cd369a28bcd4e0f49f777c0df20ae97e
vaibha987 <v2goyal95@gmail.com>
2024-07-21 17:33:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ function firstWord(s) {
     // Otherwise, return the substring up to the first space
     return s.substring(0, index);
 }
-}
+
 
 // Do not change the code below
 
-//const s = prompt("Enter String:");
-//alert(firstWord(s));
+const s = prompt("Enter String:");
+alert(firstWord(s));
