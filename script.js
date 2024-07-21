7bf002064ed76a400112299d47c5631ac7cade0f
vaibha987 <v2goyal95@gmail.com>
2024-07-21 17:30:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 function firstWord(s) {
   // your code here
+	function firstWord(str) {
+    // Trim leading and trailing whitespace
+    str = str.trim();
+    
+    // Find the index of the first space
+    let index = str.indexOf(' ');
+    
+    // If no space found, return the whole string
+    if (index === -1) {
+        return str;
+    }
+    
+    // Otherwise, return the substring up to the first space
+    return str.substring(0, index);
+}
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+//const s = prompt("Enter String:");
+//alert(firstWord(s));
