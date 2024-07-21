761d4814a5eef556f8c8546713ecf2ab43cd5af6
vaibha987 <v2goyal95@gmail.com>
2024-07-21 17:31:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 function firstWord(s) {
   // your code here
-	function firstWord(str) {
     // Trim leading and trailing whitespace
-    str = str.trim();
+    s = s.trim();
     
     // Find the index of the first space
-    let index = str.indexOf(' ');
+    let index = s.indexOf(' ');
     
     // If no space found, return the whole string
     if (index === -1) {
-        return str;
+        return s;
     }
     
     // Otherwise, return the substring up to the first space
-    return str.substring(0, index);
+    return s.substring(0, index);
 }
 }
 
